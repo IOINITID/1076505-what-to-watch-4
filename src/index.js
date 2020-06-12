ad20ef5bbf6246f7d35c3063a5c57efaa600ff36
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-06-12 13:50:32 +0300

    Added and validated components using prop-types library

--- src/index.js
+++ src/index.js
@@ -5,7 +5,30 @@ import App from './components/app/app.jsx';
 const movieData = {
   title: `The Grand Budapest Hotel`,
   genre: `Drama`,
-  releaseDate: `2014`
+  releaseDate: `2014`,
 };
 
-ReactDOM.render(<App movieData={movieData} />, document.querySelector(`#root`));
+const movieTitles = [
+  `Fantastic Beasts: The Crimes of Grindelwald`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`,
+  `We need to talk about Kevin`,
+  `What We Do in the Shadows`,
+  `Revenant`,
+  `Johnny English`,
+  `Shutter Island`,
+  `Pulp Fiction`,
+  `No Country for Old Men`,
+  `Snatch`,
+  `Moonrise Kingdom`,
+  `Seven Years in Tibet`,
+  `Midnight Special`,
+  `War of the Worlds`,
+  `Dardjeeling Limited`,
+  `Orlando`,
+  `Mindhunter`,
+  `Midnight Special`
+];
+
+ReactDOM.render(<App movieData={movieData} movieTitles={movieTitles} />, document.querySelector(`#root`));
