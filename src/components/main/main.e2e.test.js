03278832018df78f802fed3853a86db0d39e4f2d
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-06-13 21:34:48 +0300

    Update header to link element for tests

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
+import Enzyme, {mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Main from './main.jsx';
 
@@ -40,16 +40,16 @@ describe(`Main`, () => {
   it(`Click on header of movie card should be done`, () => {
     const onMovieCardClick = jest.fn();
 
-    const main = shallow(
+    const main = mount(
         <Main movieData={movieData} movieTitles={movieTitles} onMovieCardClick={onMovieCardClick}/>
     );
 
-    const movieCardHeaders = main.find(`h3.small-movie-card__title`);
+    const movieCardLinks = main.find(`a.small-movie-card__link`);
 
-    movieCardHeaders.forEach((header) => {
-      header.simulate(`click`);
+    movieCardLinks.forEach((link) => {
+      link.simulate(`click`);
     });
 
-    expect(onMovieCardClick).toHaveBeenCalledTimes(movieCardHeaders.length);
+    expect(onMovieCardClick).toHaveBeenCalledTimes(movieCardLinks.length);
   });
 });
