725befe2bd5cc4185344d943f0d5ed65de7b4518
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-06-18 17:49:45 +0300

    Update e2e test for movie-card component

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -1,67 +1,67 @@
-// import React from 'react';
-// import Enzyme, {mount} from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// import Main from './main.jsx';
+import React from 'react';
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Main from './main.jsx';
 
-// const movieData = {
-//   title: `The Grand Budapest Hotel`,
-//   genre: `Drama`,
-//   releaseDate: `2014`,
-// };
+const movieData = {
+  title: `The Grand Budapest Hotel`,
+  genre: `Drama`,
+  releaseDate: `2014`,
+};
 
-// const films = [
-//   {
-//     title: `Fantastic Beasts: The Crimes of Grindelwald`,
-//     image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
-//   },
-//   {
-//     title: `Bohemian Rhapsody`,
-//     image: `img/bohemian-rhapsody.jpg`
-//   },
-//   {
-//     title: `Macbeth`,
-//     image: `img/macbeth.jpg`
-//   },
-//   {
-//     title: `Aviator`,
-//     image: `img/aviator.jpg`
-//   },
-//   {
-//     title: `We need to talk about Kevin`,
-//     image: `img/we-need-to-talk-about-kevin.jpg`
-//   },
-//   {
-//     title: `What We Do in the Shadows`,
-//     image: `img/what-we-do-in-the-shadows.jpg`
-//   },
-//   {
-//     title: `Revenant`,
-//     image: `img/revenant.jpg`
-//   },
-//   {
-//     title: `Johnny English`,
-//     image: `img/johnny-english.jpg`
-//   },
-// ];
+const films = [
+  {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    image: `img/bohemian-rhapsody.jpg`
+  },
+  {
+    title: `Macbeth`,
+    image: `img/macbeth.jpg`
+  },
+  {
+    title: `Aviator`,
+    image: `img/aviator.jpg`
+  },
+  {
+    title: `We need to talk about Kevin`,
+    image: `img/we-need-to-talk-about-kevin.jpg`
+  },
+  {
+    title: `What We Do in the Shadows`,
+    image: `img/what-we-do-in-the-shadows.jpg`
+  },
+  {
+    title: `Revenant`,
+    image: `img/revenant.jpg`
+  },
+  {
+    title: `Johnny English`,
+    image: `img/johnny-english.jpg`
+  },
+];
 
-// Enzyme.configure({
-//   adapter: new Adapter()
-// });
+Enzyme.configure({
+  adapter: new Adapter()
+});
 
-// describe(`Main`, () => {
-//   it(`Click on header of movie card should be done`, () => {
-//     // const onMovieCardClick = jest.fn();
+describe(`Main`, () => {
+  it(`Click on header of movie card should be done`, () => {
+    const handlerMovieCardClick = jest.fn();
 
-//     const main = mount(
-//         <Main movieData={movieData} films={films}/>
-//     );
+    const main = mount(
+        <Main movieData={movieData} films={films} onMovieCardClick={handlerMovieCardClick}/>
+    );
 
-//     const movieCardLinks = main.find(`a.small-movie-card__link`);
+    const movieCardLinks = main.find(`a.small-movie-card__link`);
 
-//     movieCardLinks.forEach((link) => {
-//       link.simulate(`click`);
-//     });
+    movieCardLinks.forEach((link) => {
+      link.simulate(`click`);
+    });
 
-//     // expect(onMovieCardClick).toHaveBeenCalledTimes(movieCardLinks.length);
-//   });
-// });
+    expect(handlerMovieCardClick).toHaveBeenCalledTimes(movieCardLinks.length);
+  });
+});
--- src/components/movie-card/movie-card.e2e.test.js
+++ src/components/movie-card/movie-card.e2e.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, {mount} from 'enzyme';
+import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import MovieCard from './movie-card.jsx';
 
@@ -13,28 +13,10 @@ Enzyme.configure({
 });
 
 describe(`MovieCard`, () => {
-  it(`Click on header of movie card should be done`, () => {
-    const handlerMovieCardClick = jest.fn();
-
-    const movieCard = mount(
-        <MovieCard
-          film={film}
-          onMovieCardClick={handlerMovieCardClick}
-          onMovieCardHover={() => {}}
-        />
-    );
-
-    const movieCardItem = movieCard.find(`h3.small-movie-card__title`);
-
-    movieCardItem.simulate(`click`);
-
-    expect(handlerMovieCardClick).toHaveBeenCalledTimes(movieCardItem.length);
-  });
-
   it(`Hover on movie card should return film data`, () => {
     const handlerMovieCardHover = jest.fn();
 
-    const movieCard = mount(
+    const movieCard = shallow(
         <MovieCard
           film={film}
           onMovieCardClick={() => {}}
@@ -44,8 +26,8 @@ describe(`MovieCard`, () => {
 
     const movieCardItem = movieCard.find(`article.small-movie-card`);
 
-    movieCardItem.simulate(`mouseenter`);
+    movieCardItem.simulate(`mouseover`);
 
-    // expect(handlerMovieCardHover).toHaveBeenCalledWith(film);
+    expect(handlerMovieCardHover).toHaveBeenCalledWith(film);
   });
 });
