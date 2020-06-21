77235c01cd60b992ad1331a2b2da22121e2e0e16
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-06-21 23:49:48 +0300

    Added state for app component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,23 +3,52 @@ import PropTypes from 'prop-types';
 import Main from '../main/main.jsx';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import MovieDetails from '../movie-details/movie-details.jsx';
-import film from '../../mocks/film.js';
-
-const handlerMovieCardClick = () => {};
 
 export default class App extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      page: `main`
+    };
+
+    this.handleMovieCardClick = this.handleMovieCardClick.bind(this);
+  }
+
+  handleMovieCardClick(evt) {
+    evt.preventDefault();
+
+    this.setState({
+      page: `movieDetails`
+    });
+  }
+
+  renderApp() {
+    const {movieData, films, film} = this.props;
+    const {page} = this.state;
+
+    switch (page) {
+      case `main`:
+        return (
+          <Main movieData={movieData} films={films} onMovieCardClick={this.handleMovieCardClick}/>
+        );
+      case `movieDetails`:
+        return (
+          <MovieDetails film={film}/>
+        );
+      default:
+        return null;
+    }
   }
 
   render() {
-    const {movieData, films} = this.props;
+    const {film} = this.props;
 
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
-            <Main movieData={movieData} films={films} onMovieCardClick={handlerMovieCardClick}/>
+            {this.renderApp()}
           </Route>
           <Route exact path="/dev-film">
             <MovieDetails film={film}/>
@@ -36,5 +65,17 @@ App.propTypes = {
     genre: PropTypes.string.isRequired,
     releaseDate: PropTypes.string.isRequired
   }).isRequired,
-  films: PropTypes.array.isRequired
+  films: PropTypes.array.isRequired,
+  film: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    releaseDate: PropTypes.string.isRequired,
+    cover: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    ratingCount: PropTypes.string.isRequired,
+    description: PropTypes.array.isRequired,
+    director: PropTypes.string.isRequired,
+    starring: PropTypes.array.isRequired
+  }).isRequired
 };
