bec4841e2e01a2f6061dca1b5dd9924eec3150a2
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-06-21 23:58:20 +0300

    Added unit test for movie-details component

---
+++ src/components/movie-details/movie-details.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import rerender from 'react-test-renderer';
+import MovieDetails from '../movie-details/movie-details.jsx';
+
+const film = {
+  title: `The Grand Budapest Hotel`,
+  genre: `Drama`,
+  releaseDate: `2014`,
+  cover: `img/bg-the-grand-budapest-hotel.jpg`,
+  poster: `img/the-grand-budapest-hotel-poster.jpg`,
+  rating: 8.9,
+  ratingCount: `240`,
+  description: [
+    `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.`,
+    `Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`
+  ],
+  director: `Wes Andreson`,
+  starring: [`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe`]
+};
+
+describe(`MovieDetails`, () => {
+  it(`MovieDetails should render correctly`, () => {
+    const tree = rerender.create(<MovieDetails film={film}/>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
