f1d76eb46df849926e57460b328817e635ddad01
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-06-21 23:30:18 +0300

    Update app component to class component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import Main from '../main/main.jsx';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
@@ -7,22 +7,28 @@ import film from '../../mocks/film.js';
 
 const handlerMovieCardClick = () => {};
 
-const App = (props) => {
-  const {movieData, films} = props;
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Main movieData={movieData} films={films} onMovieCardClick={handlerMovieCardClick}/>
-        </Route>
-        <Route exact path="/dev-film">
-          <MovieDetails film={film}/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
-};
+  render() {
+    const {movieData, films} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main movieData={movieData} films={films} onMovieCardClick={handlerMovieCardClick}/>
+          </Route>
+          <Route exact path="/dev-film">
+            <MovieDetails film={film}/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   movieData: PropTypes.shape({
@@ -32,5 +38,3 @@ App.propTypes = {
   }).isRequired,
   films: PropTypes.array.isRequired
 };
-
-export default App;
